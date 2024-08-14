111aeefb1937583b2245cd2ca3cc164ebb1080cf
granaqq <156215485+grateqq@users.noreply.github.com>
2024-08-14 16:38:34 -0300

    direfenciar ganador

--- RPS.js
+++ RPS.js
@@ -3,8 +3,9 @@ let winner
 let playerSelection
 let computerSelection
 let round = 0
-
+// papper rock scissors
 // FUNCIONES
+  //1
 function getComputerChoice() {
   let ref = Math.floor(Math.random()*100)
   if (ref<33) {
@@ -15,6 +16,44 @@ function getComputerChoice() {
     return  "rock"
   }
 }
+  //2
+function playRonda(playerSelection,computerSelection) {
+    //console.log("PLAYER: "+ playerSelection +" COMPUTER: " +computerSelection )
+    if (playerSelection === computerSelection) {
+      //console.log("DRAW")
+      return "DRAW"
+    }
+    else if (playerSelection === "rock") {
+      if (computerSelection === "scissors") {
+       //console.log("WIN PLAYER") 
+       return ("You win")
+      } 
+      else if (computerSelection === "papper") {
+        //console.log("COMPUTER WIN")
+        return ("You lose")
+      }
+    }
+    else if (playerSelection === "scissors") {
+      if (computerSelection === "papper") {
+       //console.log("WIN PLAYER") 
+       return ("You win")
+      } 
+      else if (computerSelection === "rock") {
+        //console.log("COMPUTER WIN")
+        return ("You lose")
+      }
+    }
+    else if (playerSelection === "papper") {
+      if (computerSelection === "rock") {
+       //console.log("WIN PLAYER") 
+       return ("You win")
+      } 
+      else if (computerSelection === "scissors") {
+       // console.log("COMPUTER WIN")
+        return ("You lose")
+      }
+    }
+}
 
 // BOTONES
 document.getElementById("papperButton").addEventListener("click", function() {
@@ -24,16 +63,25 @@ document.getElementById("papperButton").addEventListener("click", function() {
   // Selecciones
   playerSelection = "papper"; 
   computerSelection = getComputerChoice();
-  // posteo selecciones
-  let link = document.getElementById("rondas");  // guardo en link la etiqueta a modificar
-  let textraund = document.createElement("p");
-  textraund.textContent = 
-    "Ronda " +round+ " Player: " + playerSelection 
-    + " Computer: " + computerSelection;
-  
-  link.appendChild(textraund)
+ 
     
   console.log(playerSelection);
   console.log(computerSelection);
 
+  // WINNER
+  winner = playRonda(playerSelection,computerSelection)
+  console.log(winner)
+  // posteo selecciones  y resultado
+  let link = document.getElementById("rondas");  // guardo en link la etiqueta a modificar
+  let textraund = document.createElement("p");
+  textraund.textContent = 
+    "Ronda " +round+ " Player: " + playerSelection 
+    + " Computer: " + computerSelection
+    + ". "+ winner;
+  
+  link.appendChild(textraund)
+
+  
 });
+
+
