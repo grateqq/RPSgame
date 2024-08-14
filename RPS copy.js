b35208755dfd066d25ec942eb0c41e9f7a1b07f0
granaqq <156215485+grateqq@users.noreply.github.com>
2024-08-14 16:09:00 -0300

    empezando con js

--- RPS copy.js
+++ RPS copy.js
@@ -124,6 +124,4 @@ function playGame () {
   }
 
 }
-
-
 playGame ()
--- RPS.js
+++ RPS.js
@@ -1,129 +1,39 @@
-// Juego de rps contra la pc. por consola.
-//Mensaje Inicial
-console.log("R P S")
+let score
+let winner
+let playerSelection
+let computerSelection
+let round = 0
 
-//Funcion Eleccion de PC
+// FUNCIONES
 function getComputerChoice() {
   let ref = Math.floor(Math.random()*100)
   if (ref<33) {
-    return "piedra"
+    return "papper"
   } else if (ref<66) {
-    return "tijera"
+    return "scissors"
   } else {
-    return  "papel"
+    return  "rock"
   }
 }
 
-//Funcion eleccion de Jugador
-function getPlayerChoice() {
-  let choice
-  while (true) { 
-    choice = prompt("choice", "piedra");
-      // Si el usuario cancela el prompt, vuelve a pedir la elección
-    if (choice === null || choice.trim() === "") {
-      alert("Por favor, ingresa una opción válida.");
-      continue; 
-      // Volver al inicio del bucle
-    }
+// BOTONES
+document.getElementById("papperButton").addEventListener("click", function() {
+  //contador 
+  round = round +1;
   
-      // Si la entrada es válida, salir del bucle y devolver la elección
-    return choice.toLowerCase();
-  }
-}
-//Elecciones
-  /*
-  let computer = getComputerChoice()
-  let player = getPlayerChoice()
-  console.log("Computer: " + computer)
-  console.log("Player " + player)
-  */
-// Funcion decidir ganador de ronda segun Elecciones
-
-function unaRonda(playerSelection,computerSelection) {
-  console.log("PLAYER: "+ playerSelection +" COMPUTER: " +computerSelection )
-  if (playerSelection === computerSelection) {
-    console.log("EMPATE")
-    return "empate"
-  }
-  else if (playerSelection === "piedra") {
-    if (computerSelection === "tijera") {
-     console.log("WIN PLAYER") 
-     return ("win")
-    } 
-    else if (computerSelection === "papel") {
-      console.log("COMPUTER WIN")
-      return ("lose")
-    }
-  }
-  else if (playerSelection === "tijera") {
-    if (computerSelection === "papel") {
-     console.log("WIN PLAYER") 
-     return ("win")
-    } 
-    else if (computerSelection === "piedra") {
-      console.log("COMPUTER WIN")
-      return ("lose")
-    }
-  }
-  else if (playerSelection === "papel") {
-    if (computerSelection === "piedra") {
-     console.log("WIN PLAYER") 
-     return ("win")
-    } 
-    else if (computerSelection === "tijera") {
-      console.log("COMPUTER WIN")
-      return ("lose")
-    }
-  }
-  /*
-  else if (playerSelection === null){
-    player = prompt("error, eliga nuevamente", "piedra")
-    player = player.toLowerCase()
-    unaRonda(player, computer)
-  }
-*/
-  else {
+  // Selecciones
+  playerSelection = "papper"; 
+  computerSelection = getComputerChoice();
+  // posteo selecciones
+  let link = document.getElementById("rondas");  // guardo en link la etiqueta a modificar
+  let textraund = document.createElement("p");
+  textraund.textContent = 
+    "Ronda " +round+ " Player: " + playerSelection 
+    + " Computer: " + computerSelection;
+  
+  link.appendChild(textraund)
     
-    player = prompt("error, eliga nuevamente", "piedra")
-    player = player.toLowerCase()
-    unaRonda(player, computer)
+  console.log(playerSelection);
+  console.log(computerSelection);
 
-  }  
-}
-
-// unaRonda (player, computer)
-
-//Hacer 5 rondas, sumar puntaje
-
-function playGame () {
-  console.log("PLAYGAME INICIA")
-  let puntaje = 0
-  let resultado
-  
-
- for (let i= 1; i<=5; i ++) {
-    let computer = getComputerChoice()
-    let player = getPlayerChoice()
-
-    resultado = unaRonda(player, computer )
-  
-    if (resultado === "win") {
-    puntaje = puntaje +1
-    } else if (resultado ==="lose") {
-    puntaje = puntaje -1
-    }
-    console.log("puntaje= "+ puntaje)
-  }
-  console.log("FIN DE RONDAS")
-  if (puntaje === 0) {
-    console.log("PARTIDO TERMINADO: EMPATADO")
-  }else if (puntaje >0) {
-    console.log("PARTIDO TERMINADO: PLAYER WIN")
-  } else {
-    console.log("PARTIDO TERMINADO: COMPUTADORA WIN")
-  }
-
-}
-
-
-playGame ()
+});
